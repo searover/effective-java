0f4a30dff3997c2099ba95fab3d047095b891a6b
searover <ibaiguang@gmail.com>
2018-06-02 18:38:57 +0800

    Add: Some classes and .gitkeep file.

---
+++ 1-create-and-destroy-object/src/main/java/Services.java
@@ -0,0 +1,30 @@
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Services {
+    private Services() {
+    }
+
+    private static final Map<String, Provider> providers = new ConcurrentHashMap<>();
+
+    public static final String DEFAULT_PROVIDER_NAME = "<def>";
+
+    public static void registerProvider(Provider p) {
+        providers.put(DEFAULT_PROVIDER_NAME, p);
+    }
+
+    public static void registerProvider(String name, Provider p) {
+        providers.put(name, p);
+    }
+
+    public static Service newInstance() {
+        return providers.get(DEFAULT_PROVIDER_NAME).newService();
+    }
+
+    public static Service newInstance(String name) {
+        Provider p = providers.get(name);
+        if (p == null)
+            throw new IllegalArgumentException("No provider registerd with name: " + name);
+        return p.newService();
+    }
+}
