a76e6fdcef12fd850018375798245ddac8455225
searover <ibaiguang@gmail.com>
2019-04-14 11:05:30 +0800

    Add Tuples.

---
+++ basic/src/main/java/net/mindview/util/ThreeTuple.java
@@ -0,0 +1,19 @@
+package net.mindview.util;
+
+public class ThreeTuple<A, B, C> extends TwoTuple<A, B> {
+    private final C third;
+
+    public ThreeTuple(A first, B second, C c) {
+        super(first, second);
+        third = c;
+    }
+
+    @Override
+    public String toString() {
+        return "ThreeTuple{" +
+                "third=" + third +
+                ", first=" + first +
+                ", second=" + second +
+                '}';
+    }
+}
---
+++ basic/src/main/java/net/mindview/util/TwoTuple.java
@@ -0,0 +1,19 @@
+package net.mindview.util;
+
+public class TwoTuple<A, B> {
+    public final A first;
+    public final B second;
+
+    public TwoTuple(A first, B second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    @Override
+    public String toString() {
+        return "TwoTuple{" +
+                "first=" + first +
+                ", second=" + second +
+                '}';
+    }
+}
