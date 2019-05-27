615c0e7230d820e8529e3e12c5f806368fac002a
searover <ibaiguang@gmail.com>
2019-05-28 03:29:19 +0800

    Good use of hashcode.

---
+++ basic/src/main/java/net/mindview/containers/CountedString.java
@@ -0,0 +1,58 @@
+package net.mindview.containers;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author searover
+ * Creating a good hashCode().
+ */
+public class CountedString {
+    private static List<String> created = new ArrayList<>();
+    private String s;
+    private int id = 0;
+
+    public CountedString(String str) {
+        s = str;
+        created.add(s);
+        for (String c : created) {
+            if (c.equals(s)) {
+                id++;
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "String: " + s + " id: " + id + " hashCode(): " + hashCode();
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 37 * result + s.hashCode();
+        result = 37 * result + id;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return obj instanceof CountedString && s.equals(((CountedString) obj).s) && id == ((CountedString) obj).id;
+    }
+
+    public static void main(String[] args) {
+        Map<CountedString, Integer> map = new HashMap<>();
+        CountedString[] cs = new CountedString[5];
+        for (int i = 0; i < cs.length; i++) {
+            cs[i] = new CountedString("hi");
+            map.put(cs[i], i);
+        }
+        System.out.println(map);
+        for (CountedString c : cs) {
+            System.out.println("Looking up " + c);
+            System.out.println(map.get(c));
+        }
+    }
+}
