6bdd6722f8718ba5a19ee3281eef4de8cedb2260
searover <ibaiguang@gmail.com>
2019-06-01 07:43:00 +0800

    线程中的异常捕获

---
+++ basic/src/main/java/net/mindview/concurrency/ResponsiveUI.java
@@ -0,0 +1,36 @@
+package net.mindview.concurrency;
+
+import java.io.IOException;
+
+public class ResponsiveUI extends Thread {
+    private static volatile double d = 1;
+
+    public ResponsiveUI() {
+        setDaemon(true);
+        start();
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            d = d + (Math.PI + Math.E) / d;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        new ResponsiveUI();
+        System.in.read();
+        System.out.println(d);
+    }
+}
+
+class UnresponsiveUI {
+    private volatile double d = 1;
+
+    public UnresponsiveUI() throws Exception {
+        while ((d > 0)) {
+            d = d + (Math.PI + Math.E) / d;
+        }
+        System.in.read();
+    }
+}
