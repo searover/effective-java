efd670545ac57c48faa37b9472f0359d2dfdec49
searover <ibaiguang@gmail.com>
2019-06-04 22:32:06 +0800

    对阻塞线程的中断

--- basic/src/main/java/net/mindview/concurrency/Interrupting.java
+++ basic/src/main/java/net/mindview/concurrency/Interrupting.java
@@ -13,14 +13,14 @@ public class Interrupting {
     static void test(Runnable r) throws InterruptedException {
         Future<?> f = exec.submit(r);
         TimeUnit.MILLISECONDS.sleep(100);
-        System.out.println("Interrupting " + r.getClass().getName());
+        System.out.println("Interrupting " + r.getClass().getSimpleName());
         f.cancel(true);
-        System.out.println("Interrupt sent to " + r.getClass().getName());
+        System.out.println("Interrupt sent to " + r.getClass().getSimpleName());
     }
 
     public static void main(String[] args) throws InterruptedException {
-//        test(new SleepBlocked());
-//        test(new IOBlocked(System.in));
+        test(new SleepBlocked());
+        test(new IOBlocked(System.in));
         test(new SynchronizedBlocked());
         TimeUnit.SECONDS.sleep(3);
         System.out.println("Aborting with System.exit(0)");
