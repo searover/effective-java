6e92a8d4c3827d7df4b39244e2648805eacd2b83
searover <ibaiguang@gmail.com>
2022-11-08 23:01:02 +0800

    Add: Template-Method pattern.

---
+++ dp/src/main/java/io/example/template/TestPaperA.java
@@ -0,0 +1,22 @@
+package io.example.template;
+
+/**
+ * @author luxz
+ * @date 2022/11/8-22:57
+ */
+public class TestPaperA extends TestPaper {
+    @Override
+    public String Answer1() {
+        return "a";
+    }
+
+    @Override
+    public String Answer2() {
+        return "b";
+    }
+
+    @Override
+    public String Answer3() {
+        return "c";
+    }
+}
